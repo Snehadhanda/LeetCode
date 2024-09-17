fa8cff9255ac72363fb9bc561b34578358d0aaf3
Sneha Dhanda <144700446+Snehadhanda@users.noreply.github.com>
2024-09-17 19:59:54 +0530

    Time: 9 ms (7.89%), Space: 13.7 MB (9.22%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -8,18 +8,41 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+//ITERATION
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         ListNode* prev = nullptr;
+//         ListNode*curr = head;
+//         while(curr != nullptr){
+//             ListNode*nextNode = curr->next; //taki track lost na ho
+//             curr->next = prev;
+//             prev = curr;
+//             curr = nextNode;
+            
+//         }
+//         return prev;  //curr isiliye return nhi kra kuki curr end me null pe aayega
+//     } 
+// };
+
+//RECURSION
 class Solution {
 public:
+    ListNode* reverse(ListNode* &prev,ListNode* &curr){
+        if(curr == nullptr){
+            return prev;
+        }
+        //1 case hum solve kre ge baki recursion sambhale ga
+        ListNode*nextNode = curr->next;
+        curr->next = prev;
+        //baki recursion sambhale ga
+        ListNode* ans = reverse(curr,nextNode);
+        return ans;
+    }
     ListNode* reverseList(ListNode* head) {
         ListNode* prev = nullptr;
-        ListNode*curr = head;
-        while(curr != nullptr){
-            ListNode*nextNode = curr->next; //taki track lost na ho
-            curr->next = prev;
-            prev = curr;
-            curr = nextNode;
-            
-        }
-        return prev;
+        ListNode* curr = head;
+        return reverse(prev,curr);
     }
 };
