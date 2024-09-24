9f7ee38fe4d04db385f2663816b24723ef4ca143
Sneha Dhanda <144700446+Snehadhanda@users.noreply.github.com>
2024-09-24 21:53:07 +0530

    Time: 4 ms (92.62%), Space: 15.4 MB (58.50%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -8,47 +8,74 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+//THIS SOL WILL TAKE TC-O(N) AND SC- O(N)
+// class Solution {
+// public:
+//     void OddEven(ListNode* &head, vector<int> &arr){
+//         ListNode*temp = head;
+//         while(temp != nullptr){
+//             arr.push_back(temp->val);
+//             temp = temp->next;
+//             if(temp != nullptr){
+//                 temp = temp->next;
+//             }
+//         }
+//         if(temp) arr.push_back(temp->val);
+//         if(head->next != nullptr){
+//             temp = head->next;
+//             while(temp!=nullptr){
+//             arr.push_back(temp->val);
+//             temp = temp->next;
+//             if(temp != nullptr){
+//                 temp = temp->next;
+//                 }
+//             }
+//             if(temp) arr.push_back(temp->val);
+//         }
+//     }
+//     ListNode* oddEvenList(ListNode* head) {
+//         ListNode* temp = head;
+//         vector<int>arr;
+//         if(head == nullptr){
+//             return nullptr;
+//         }
+//         if(head->next == nullptr){
+//             return head;
+//         }
+//         OddEven(head,arr);
+//         int i=0;
+//         temp = head;
+//         while(temp != nullptr && i<arr.size()){
+//             temp->val = arr[i];
+//             i++;
+//             temp = temp->next;
+//         }
+//         return head;
+//     }
+// };
+
+
+//OPTIMISED SOLUTION:
 class Solution {
 public:
-    void OddEven(ListNode* &head, vector<int> &arr){
-        ListNode*temp = head;
-        while(temp != nullptr){
-            arr.push_back(temp->val);
-            temp = temp->next;
-            if(temp != nullptr){
-                temp = temp->next;
-            }
-        }
-        if(temp) arr.push_back(temp->val);
-        if(head->next != nullptr){
-            temp = head->next;
-            while(temp!=nullptr){
-            arr.push_back(temp->val);
-            temp = temp->next;
-            if(temp != nullptr){
-                temp = temp->next;
-                }
-            }
-            if(temp) arr.push_back(temp->val);
-        }
-    }
     ListNode* oddEvenList(ListNode* head) {
-        ListNode* temp = head;
-        vector<int>arr;
         if(head == nullptr){
             return nullptr;
         }
         if(head->next == nullptr){
             return head;
         }
-        OddEven(head,arr);
-        int i=0;
-        temp = head;
-        while(temp != nullptr && i<arr.size()){
-            temp->val = arr[i];
-            i++;
-            temp = temp->next;
-        }
+        ListNode* odd = head;
+        ListNode*even = head->next;
+        ListNode*evenHead = head->next;
+        while(even != nullptr && even->next != nullptr){
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+            odd = odd->next; // hmne odd->next isiliye likha kuki hum odd->next ko alrdy point krva chuke hai
+            even = even->next;
+    }
+        odd->next = evenHead;
         return head;
     }
 };
