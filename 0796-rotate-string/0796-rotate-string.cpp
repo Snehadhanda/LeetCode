36c80330d792003e3f6685baea34f23823b5cb69
Sneha Dhanda <144700446+Snehadhanda@users.noreply.github.com>
2024-11-26 23:40:21 +0530

    Time: 2 ms (12.93%), Space: 8.1 MB (20.85%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        //if we concatenate the string s with itself then it will have all possible rotations
-        int n = s.length();
+        // //if we concatenate the string s with itself then it will have all possible rotations
+        // int n = s.length();
+        // int m = goal.length();
+        // if(m != n){
+        //     return false;
+        // }
+        // string str = s+s;
+        // if(str.find(goal)!=string::npos){
+        //     return true;
+        // }
+        // else return false;
+        
+        
+        //METHOD 2:
+        int n=s.length();
         int m = goal.length();
-        if(m != n){
-            return false;
+        for(int i=0; i<n; i++){
+            rotate(s.begin(), s.begin()+1, s.end());
+            if(s==goal){
+                return true;
+            }
         }
-        string str = s+s;
-        if(str.find(goal)!=string::npos){
-            return true;
-        }
-        else return false;
+        return false;
     }
 };
