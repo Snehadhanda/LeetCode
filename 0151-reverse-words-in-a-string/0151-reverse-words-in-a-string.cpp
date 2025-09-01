532f807143c6e66520a79afa08f59c860bb85a88
Sneha Dhanda <144700446+Snehadhanda@users.noreply.github.com>
2025-09-02 00:09:36 +0530

    Time: 2 ms (47.42%), Space: 10.8 MB (45.07%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,54 +1,28 @@
-// class Solution {
-// public:
-//     string reverseWords(string s) {
-//         s+=" "; //space is added so that last word of the string is also processed 
-//         stack<string>st;
-//         string str = "";
-//         for(int i=0; i<s.length(); i++){
-//             if(s[i]==' '){
-//                 st.push(str);
-//                 str = "";
-//             }
-//             else str+=s[i];
-//         }
-//         string ans = "";
-//         while(st.size() != 1){
-//             ans +=st.top()+" ";
-//             st.pop();
-//         }
-//         ans+=st.top();
-//         return ans;
-//     }
-// };
 class Solution {
 public:
-    string reverseWords(string s){
-        int left =0;
-        int right = s.length()-1;
-        string temp="";
-        string ans = "";
-        while(left<=right){
-            char ch = s[left];
-            if(ch != ' '){
-                temp +=ch;
-            }
-            else if(ch == ' '){
-                if(temp != ""){
-                if(ans != ""){
-                    ans = temp +' '+ans;
+    string reverseWords(string s) {
+        //add a space at end so that last word can be processed
+        s+=' ';
+        stack<string>st;
+        string str = "";
+        for(int i=0; i<s.length(); i++){
+            if(s[i] ==' '){
+                if(!str.empty()){
+                    st.push(str);
+                    str = "";
                 }
-                else{
-                    ans = temp;
-                }
-                temp="";
+                
             }
+            else{
+                str+=s[i];
             }
-            left++;
         }
-        if(temp !=""){
-            if(ans!="") ans= temp+' '+ans;
-            else ans = temp;
+        string ans ="";
+        while(st.size()>1){
+            ans+=st.top() + " ";
+            st.pop();
         }
+        ans+=st.top();
         return ans;
     }
 };
