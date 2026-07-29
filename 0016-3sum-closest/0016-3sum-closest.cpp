51f328e3eb119afc6638977469771c0aadb1cf02
Sneha Dhanda <144700446+Snehadhanda@users.noreply.github.com>
2026-07-29 23:19:05 +0530

    Time: 7 ms (86.98%), Space: 13.9 MB (83.13%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,32 +1,38 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        int n = nums.size();
-        int i = 0;
         sort(nums.begin(), nums.end());
-        int closesum = nums[0]+nums[1]+nums[2];
-        int mindiff = abs(closesum - target);
-        
-        for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
-            int l = i + 1;
-            int r = n - 1;
-            while (l < r) {
-                int sum = nums[i] + nums[l] + nums[r];
-                int currDiff = abs(sum-target);
-                if(currDiff < mindiff){
-                    mindiff = currDiff;
-                    closesum = sum;
-                }
-                if (sum == target)
+        int res;
+        int min_dif = INT_MAX;
+
+        for(int i=0; i<nums.size()-2; i++){
+            int l=i+1;
+            int r=nums.size()-1;
+
+            while(l<r){
+                int sum = nums[i]+nums[l]+nums[r];
+
+                if(sum == target){
                     return sum;
-                else if (sum < target)
+                }
+                else if(sum < target){
+                    int diff = abs(sum-target);
+                    if(diff < min_dif){
+                        min_dif = diff;
+                        res = sum;
+                    }
                     l++;
-                else
+                }
+                else{
+                    int diff = abs(sum-target);
+                    if(diff < min_dif){
+                        min_dif = diff;
+                        res = sum;
+                    }
                     r--;
+                }
             }
         }
-        return closesum;
+        return res;
     }
 };
